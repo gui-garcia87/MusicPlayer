104aea622915030e38492f201f5624d1be9a3190
Guilherme <guifavoreto@yahoo.com.br>
2016-11-16 11:28:32 -0200

    add setOnCompletionListener

--- app/src/main/java/com/example/guilherme/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/guilherme/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I'm done",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
