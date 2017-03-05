49813c33102ff55f8a65e60385465a43c0b334f5
Guilherme <guifavoreto@yahoo.com.br>
2017-03-05 20:44:40 -0300

    Merge branch 'playerActivity'
    # Conflicts:
    #	app/src/main/res/raw/song.mp3

--- app/src/main/java/com/example/guilherme/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/guilherme/musicplayer/MainActivity.java
@@ -1,57 +1,32 @@
 package com.example.guilherme.musicplayer;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        Button button = (Button) findViewById(R.id.go_player_button);
 
-        Button playButton = (Button)findViewById(R.id.play_button);
-
-        playButton.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mediaPlayer.start();
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Toast.makeText(MainActivity.this, "I'm done",Toast.LENGTH_SHORT).show();
-                    }
-                });
+
+                Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
+
+                startActivity(intent);
+
             }
         });
 
-
-
-        Button pauseButton = (Button)findViewById(R.id.pause_button);
-
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.stop();
-            }
-        });
-
-        Button resetButton = (Button)findViewById(R.id.reset_button);
-
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.reset();
-            }
-        });
 
     }
 }
